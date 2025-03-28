15c393e96f3b1b93cdf7df6c611d5639528952e7
manojlovska <annastasijamanojlovska@gmail.com>
2025-03-28 10:39:24 +0100

    Trdina, changed milestone to 5 in train_ddpm.py

--- train_ddpm.py
+++ train_ddpm.py
@@ -3,7 +3,7 @@ from train_utils import Train
 images_folder = "/ceph/grid/home/am6417/Datasets/FFHQ/images1024x1024" # "/home/anastasija/Datasets/FFHQ/images1024x1024"
 results_folder = "./results"
 project_name = "denoising-diffusion-pytorch"
-milestone = 4
+milestone = 5
 
 trainer = Train(images_folder=images_folder,
                 results_folder=results_folder,
