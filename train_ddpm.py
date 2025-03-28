16f0feca28881d0200ac580040422d3ceacf8a0f
manojlovska <annastasijamanojlovska@gmail.com>
2025-03-28 10:55:47 +0100

    Commiting before pulling from remote (Gauss)

--- train_ddpm.py
+++ train_ddpm.py
@@ -1,6 +1,6 @@
 from train_utils import Train
 
-images_folder = "/ceph/grid/home/am6417/Datasets/FFHQ/images1024x1024" # "/home/anastasija/Datasets/FFHQ/images1024x1024"
+images_folder = "/home/anastasija/Datasets/FFHQ/images1024x1024" # "/ceph/grid/home/am6417/Datasets/FFHQ/images1024x1024" # 
 results_folder = "./results"
 project_name = "denoising-diffusion-pytorch"
 milestone = 4
